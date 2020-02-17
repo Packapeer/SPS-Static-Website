52077c79610c5bae40f9df15adcec5a9e12816ae
Shabeersha <shabeersha786786@gmail.com>
2020-02-18 03:27:05 +0530

    final : Application form modified

---
+++ application/js/main.js
@@ -0,0 +1,41 @@
+$(function(){
+	$("#wizard").steps({
+        headerTag: "h2",
+        bodyTag: "section",
+        transitionEffect: "fade",
+        enableAllSteps: true,
+        transitionEffectSpeed: 500,
+        labels: {
+            finish: "Submit",
+            next: "Forward",
+            previous: "Backward"
+        }
+    });
+    $('.wizard > .steps li a').click(function(){
+    	$(this).parent().addClass('checked');
+		$(this).parent().prevAll().addClass('checked');
+		$(this).parent().nextAll().removeClass('checked');
+    });
+    // Custome Jquery Step Button
+    $('.forward').click(function(){
+    	$("#wizard").steps('next');
+    })
+    $('.backward').click(function(){
+        $("#wizard").steps('previous');
+    })
+    // Select Dropdown
+    $('html').click(function() {
+        $('.select .dropdown').hide(); 
+    });
+    $('.select').click(function(event){
+        event.stopPropagation();
+    });
+    $('.select .select-control').click(function(){
+        $(this).parent().next().toggle();
+    })    
+    $('.select .dropdown li').click(function(){
+        $(this).parent().toggle();
+        var text = $(this).attr('rel');
+        $(this).parent().prev().find('div').text(text);
+    })
+})
